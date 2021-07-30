38e802fed7660392efee72b420d7c1621943e67e
T U <tylerletendre98@gmail.com>
2021-07-30 18:02:02 -0400

    added some styling to the nav bar

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,17 +4,43 @@ import NavBar from "../Navbar/navBar";
 
 const Header = () => {
   return (
-    <header class="header-navbar">
-      <div className="header">
-        <div className="">
-          <h1>Moreno construction</h1>
-        </div>
-        <div className="navbar">
-          <NavBar />
+    <div>
+      <header className="header">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div className="comp-name">
+              <div>
+                <img src="https://www.bing.com/th?id=OIP.XRMj_ER-HEiZpB07t0KmGAHaE8&w=192&h=133&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
+              </div>
+              <h1>
+                Moreno construction
+              </h1>
+            </div>
+          </div>
+          <div class="col-6">
+          </div>
+          <div class="col-3">
+            <nav>
+              <ul>
+                <li>
+                  things
+                </li>
+                <li>
+                  things
+                </li>
+                <li>
+                  things
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </header>
+      </header>
+    </div>
   );
-};
+}
+  
 
-export default Header;
+  export default Header;
